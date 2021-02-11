41e77edee16ab963e979a7697c138b169aa5c097
Benny Blockchain <ben@styng.social>
2021-02-11 18:00:39 -0500

    Added bootstrap support

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
-
+import "../node_modules/bootstrap/scss/bootstrap.scss";
 import "../styles/global.scss";
+import "../styles/custom.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
