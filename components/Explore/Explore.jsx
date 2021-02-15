ef0064330e9bc287fc8ca4f472d73078fd3d5686
Benny Blockchain <ben@styng.social>
2021-02-15 18:10:36 -0500

    Added explore page buttons and styled them

--- components/Explore/Explore.jsx
+++ components/Explore/Explore.jsx
@@ -1,17 +1,50 @@
-import { Container, Button } from "react-bootstrap";
+import Link from "next/link";
+import styles from "./Explore.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouseUser,
+  faUserCheck,
+  faUserFriends,
+  faAddressCard,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { Container } from "react-bootstrap";
 
 const Explore = () => {
   return (
-    <Container className="p-0 h-100 w-25 position-fixed">
-      <Button className="w-100" variant="primary" size="lg">
-        Home
-      </Button>
-      <Button className="w-100" variant="primary" size="lg">
-        Friends
-      </Button>
-      <Button className="w-100" variant="primary" size="lg">
-        Communities
-      </Button>
+    <Container
+      className={`d-flex flex-column align-items-start h-100 w-25 p-auto position-fixed ${styles.container}`}
+    >
+      <Link to="/" href="#">
+        <a className={`${styles.button} d-flex align-items-center`}>
+          <FontAwesomeIcon icon={faHouseUser} className="d-inline mx-3" />
+          <div className="d-inline mx-3">Home</div>
+        </a>
+      </Link>
+      <Link to="/" href="#">
+        <a className={`${styles.button} d-flex align-items-center`}>
+          <FontAwesomeIcon icon={faUserFriends} className="d-inline mx-3" />
+          <div className="d-inline mx-3">Friends</div>
+        </a>
+      </Link>
+      <Link to="/" href="#">
+        <a className={`${styles.button} d-flex align-items-center`}>
+          <FontAwesomeIcon icon={faUserCheck} className="d-inline mx-3" />
+          <div className="d-inline mx-3">Subscriptions</div>
+        </a>
+      </Link>
+      <Link to="/" href="#">
+        <a className={`${styles.button} d-flex align-items-center`}>
+          <FontAwesomeIcon icon={faUsers} className="d-inline mx-3" />
+          <div className="d-inline mx-3">Communities</div>
+        </a>
+      </Link>
+      <Link to="/" href="#">
+        <a className={`${styles.button} d-flex align-items-center`}>
+          <FontAwesomeIcon icon={faAddressCard} className="d-inline mx-3" />
+          <div className="d-inline mx-3">Preferences</div>
+        </a>
+      </Link>
     </Container>
   );
 };
