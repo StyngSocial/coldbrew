af2578bbedf74d9a5e6968475d5235a491d95a64
Benny Blockchain <ben@styng.social>
2021-02-15 18:12:36 -0500

    Promoted sidebar staging

--- components/Promoted/Promoted.jsx
+++ components/Promoted/Promoted.jsx
@@ -1,15 +1,16 @@
+import styles from "./Promoted";
 import { Container, Button } from "react-bootstrap";
 
 const Promoted = () => {
   return (
     <Container className="p-0 h-100 w-25 position-fixed">
-      <Button className="w-100" variant="primary" size="lg">
+      <Button className="w-100" variant="senondary" size="lg">
         Ad
       </Button>
       <Button className="w-100" variant="primary" size="lg">
         cold brew
       </Button>
-      <Button className="w-100" variant="primary" size="lg">
+      <Button className="w-100" variant="secondary" size="lg">
         Influencer
       </Button>
     </Container>
