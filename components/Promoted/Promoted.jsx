6ef4e9b40efa5950c6586add390a694ff2903648
Benny Blockchain <ben@styng.social>
2021-02-15 18:50:20 -0500

    Boiler plate card on Promoted, Responsive promoted

--- components/Promoted/Promoted.jsx
+++ components/Promoted/Promoted.jsx
@@ -1,18 +1,26 @@
-import styles from "./Promoted";
-import { Container, Button } from "react-bootstrap";
+import styles from "./Promoted.module.scss";
+import { Container, Card } from "react-bootstrap";
 
 const Promoted = () => {
   return (
-    <Container className="p-0 h-100 w-25 position-fixed">
-      <Button className="w-100" variant="senondary" size="lg">
-        Ad
-      </Button>
-      <Button className="w-100" variant="primary" size="lg">
-        cold brew
-      </Button>
-      <Button className="w-100" variant="secondary" size="lg">
-        Influencer
-      </Button>
+    <Container
+      className={`d-flex flex-column align-items-start h-100 w-25 position-fixed ${styles.container}`}
+    >
+      <h2>Promoted</h2>
+      <Card style={{ width: "100%" }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Card Subtitle
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
