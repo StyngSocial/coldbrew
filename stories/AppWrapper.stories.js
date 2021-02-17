9fd8d1798d7f68b244a84eb06e7b1c99227e7a67
Benny Blockchain <ben@styng.social>
2021-02-16 20:36:44 -0500

    Removed storybook

--- stories/AppWrapper.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AppWrapper from "../components/layouts/AppWrapper";
-
-export default {
-  title: "MainLayout Layout",
-  component: AppWrapper,
-};
-
-export const AppWrapperStory = () => <AppWrapper />;
--- stories/Header.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import WebHeader from "../components/Layout/WebHeader.jsx";
-
-export default {
-  title: "Header",
-  component: WebHeader,
-};
-
-export const HeaderStory = () => <WebHeader />;
