36d131e8397298002e78ca45463921574e3a66ef
Benny Blockchain <ben@styng.social>
2021-02-18 11:14:34 -0500

    Refactored Eplore page

--- components/Explore/Explore.jsx
+++ components/Explore/Explore.jsx
@@ -12,36 +12,34 @@ import { Container } from "react-bootstrap";
 
 const Explore = () => {
   return (
-    <Container
-      className={`d-flex flex-column align-items-start h-100 w-25 p-auto position-fixed ${styles.container}`}
-    >
+    <Container className={`p-0 mt-4 d-flex flex-column align-items-start`}>
       <Link to="/" href="#">
         <a className={`${styles.button} d-flex align-items-center`}>
-          <FontAwesomeIcon icon={faHouseUser} className="d-inline mx-3" />
+          <FontAwesomeIcon icon={faHouseUser} className="d-inline" />
           <div className="d-inline mx-3">Home</div>
         </a>
       </Link>
       <Link to="/" href="#">
         <a className={`${styles.button} d-flex align-items-center`}>
-          <FontAwesomeIcon icon={faUserFriends} className="d-inline mx-3" />
+          <FontAwesomeIcon icon={faUserFriends} className="d-inline" />
           <div className="d-inline mx-3">Friends</div>
         </a>
       </Link>
       <Link to="/" href="#">
         <a className={`${styles.button} d-flex align-items-center`}>
-          <FontAwesomeIcon icon={faUserCheck} className="d-inline mx-3" />
+          <FontAwesomeIcon icon={faUserCheck} className="d-inline" />
           <div className="d-inline mx-3">Subscriptions</div>
         </a>
       </Link>
       <Link to="/" href="#">
         <a className={`${styles.button} d-flex align-items-center`}>
-          <FontAwesomeIcon icon={faUsers} className="d-inline mx-3" />
+          <FontAwesomeIcon icon={faUsers} className="d-inline" />
           <div className="d-inline mx-3">Communities</div>
         </a>
       </Link>
       <Link to="/" href="#">
         <a className={`${styles.button} d-flex align-items-center`}>
-          <FontAwesomeIcon icon={faAddressCard} className="d-inline mx-3" />
+          <FontAwesomeIcon icon={faAddressCard} className="d-inline" />
           <div className="d-inline mx-3">Preferences</div>
         </a>
       </Link>
