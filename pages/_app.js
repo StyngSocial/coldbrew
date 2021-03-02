441d30cd95a9103c98b7cf520ca2af8c2c5333bc
Benny Blockchain <ben@styng.social>
2021-03-02 10:15:33 -0500

    removed next-auth and others

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import { Provider } from "next-auth/client";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.scss";
 import "../styles/custom.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,18 @@
 import Head from "next/head";
-import { useSession } from "next-auth/client";
 import AppWrapper from "../layouts/app/AppWrapper";
 import HomeFeed from "../components/HomeFeed/HomeFeed";
-import Login from "../components/Login/Login.jsx";
-import Nav from "../components/Nav/Nav";
+// import Login from "../components/Login/Login.jsx";
 
 export default function Home() {
-  const [session] = useSession();
   return (
     <>
       <Head>
         <title>Cold Brew</title>
       </Head>
-      {!session && (
-        <>
-          <Nav />
-          <Login />
-        </>
-      )}
-      {session && (
-        <AppWrapper>
-          <HomeFeed />
-        </AppWrapper>
-      )}
+
+      <AppWrapper>
+        <HomeFeed />
+      </AppWrapper>
     </>
   );
 }
