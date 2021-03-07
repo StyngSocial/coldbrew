9c36fdd69b87dba7d07e788ad86ce2c01cba5113
Benny Blockchain <ben@styng.social>
2021-03-06 19:47:56 -0500

    Fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
