0b3144a73860f474eed4d9af339b86a35d4da30a
Benny Blockchain <ben@styng.social>
2021-03-19 18:24:34 -0400

    TypeScript API. Started it, but I should just be using dHive.

---
+++ pages/api/hive/communities/index.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import hive from "@hiveio/hive-js";
+
+type PopCommunity = {
+  category: String;
+  name: String;
+};
+type PopCommunitiesRes = Array<PopCommunity>;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { limit } = req.query;
+  const params = {
+    limit,
+  };
+  hive.api
+    .callAsync("bridge.list_pop_communities", params)
+    .then((resp: Array<any>) => {
+      const popCommunities: PopCommunitiesRes = [];
+      resp.map((hivePopCommunities) => {
+        const comObj: PopCommunity = {
+          category: hivePopCommunities[0],
+          name: hivePopCommunities[1],
+        };
+        popCommunities.push(comObj);
+      });
+      res.status(200).send(popCommunities);
+    })
+    .catch((err: Error) => res.status(500).json(err));
+}
